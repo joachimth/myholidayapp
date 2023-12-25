5141f446e6b1c0087c91414c97623165e5b5bf69
Joachim Thirsbro <joachim@thirsbro.dk>
2023-12-26 00:27:12 +0100

    HolidayList.js Update

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const HolidayList = () => {
-    const [year, setYear] = useState(new Date().getFullYear());
+    const currentYear = new Date().getFullYear();
+    const [year, setYear] = useState(currentYear);
     const [holidays, setHolidays] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -16,21 +17,25 @@ const HolidayList = () => {
     }, [year]);
 
     return (
-        <div>
-            <h1>Danske Helligdage i {year}</h1>
-            <select value={year} onChange={e => setYear(e.target.value)}>
-                {/* Option-elementer for år */}
-            </select>
-            {loading ? <p>Loading...</p> : (
-                <ul>
-                    {holidays.map(holiday => (
-                        <li key={holiday.date}>
-                            {holiday.date} - {holiday.name}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
+        <div className="container mx-auto p-4">
+    <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">Danske Helligdage i {year}</h1>
+    <div className="mb-4">
+        <select className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500" value={year} onChange={e => setYear(e.target.value)}>
+            <option value={currentYear}>{currentYear}</option>
+            <option value={currentYear + 1}>{currentYear + 1}</option>
+            <option value={currentYear + 2}>{currentYear + 2}</option>
+        </select>
+    </div>
+    {loading ? <p>Loading...</p> : (
+        <ul className="list-disc list-inside">
+            {holidays.map(holiday => (
+                <li key={holiday.date} className="mb-2">
+                    {holiday.localName} - {holiday.date}
+                </li>
+            ))}
+        </ul>
+    )}
+</div>
     );
 };
 
