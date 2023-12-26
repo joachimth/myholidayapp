a156f12146bcae542bb17b7def45b3caf72994a2
Joachim Thirsbro <joachim@thirsbro.dk>
2023-12-26 20:12:16 +0100

    Footer.js updated with correct export

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import axios from 'axios';
-import { Menu, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/24/solid'; // for solid stil
+import React from 'react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -10,5 +7,7 @@ const Footer = () => {
     <footer className="bg-gray-200 text-center text-sm py-3">
       <p>Repo: <a href="https://github.com/joachimth/MyHolidayApp" className="text-blue-600 hover:text-blue-800">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-blue-600 hover:text-blue-800">joachimth</a> | Senest Opdateret: {currentYear}</p>
     </footer>
-    
- export default Footer;
+  );
+};
+
+export default Footer;
