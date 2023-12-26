78633e356b99896600549b6e516fe4e31176cad1
Joachim Thirsbro <joachim@thirsbro.dk>
2023-12-26 15:25:10 +0100

    HolidayList.js Update style

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -1,42 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 const HolidayList = () => {
-    const currentYear = new Date().getFullYear();
-    const [year, setYear] = useState(currentYear);
-    const [holidays, setHolidays] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const currentYear = new Date().getFullYear();
+  const [year, setYear] = useState(currentYear);
+  const [holidays, setHolidays] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year}/DK`)
-            .then(response => {
-                setHolidays(response.data);
-                setLoading(false);
-            })
-            .catch(error => console.error('Error:', error));
-    }, [year]);
+  useEffect(() => {
+    axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year}/DK`)
+      .then(response => {
+        setHolidays(response.data);
+        setLoading(false);
+      })
+      .catch(error => console.error('Error:', error));
+  }, [year]);
 
-    return (
-        <div className="container mx-auto p-4">
-    <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">Danske Helligdage i {year}</h1>
-    <div className="mb-4">
-        <select className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500" value={year} onChange={e => setYear(e.target.value)}>
-            <option value={currentYear}>{currentYear}</option>
-            <option value={currentYear + 1}>{currentYear + 1}</option>
-            <option value={currentYear + 2}>{currentYear + 2}</option>
-        </select>
-    </div>
-    {loading ? <p>Loading...</p> : (
+  return (
+    <div className="container mx-auto p-4">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            {year}
+            <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {[currentYear, currentYear + 1, currentYear + 2].map(yr => (
+                <Menu.Item key={yr}>
+                  {({ active }) => (
+                    <button
+                      onClick={() => setYear(yr)}
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block w-full px-4 py-2 text-left text-sm'
+                      )}
+                    >
+                      {yr}
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+
+      <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">Danske Helligdage i {year}</h1>
+      {loading ? <p>Loading...</p> : (
         <ul className="list-disc list-inside">
-            {holidays.map(holiday => (
-                <li key={holiday.date} className="mb-2">
-                    {holiday.localName} - {holiday.date}
-                </li>
-            ))}
+          {holidays.map(holiday => (
+            <li key={holiday.date} className="mb-2">
+              {holiday.localName} - {holiday.date}
+            </li>
+          ))}
         </ul>
-    )}
-</div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default HolidayList;
