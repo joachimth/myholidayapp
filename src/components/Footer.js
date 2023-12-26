7ff0b1f058c72a5b33379c88189ac50d73d8ec3b
Joachim Thirsbro <joachim@thirsbro.dk>
2023-12-26 20:03:37 +0100

    HolidayList.js updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/24/solid'; // for solid stil
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -7,4 +10,5 @@ const Footer = () => {
     <footer className="bg-gray-200 text-center text-sm py-3">
       <p>Repo: <a href="https://github.com/joachimth/MyHolidayApp" className="text-blue-600 hover:text-blue-800">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-blue-600 hover:text-blue-800">joachimth</a> | Senest Opdateret: {currentYear}</p>
     </footer>
- 
+    
+ export default Footer;
