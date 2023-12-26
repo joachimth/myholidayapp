2a239912eaebcecb09ff812472cffc40b2af905d
Joachim Thirsbro <joachim@thirsbro.dk>
2023-12-26 22:24:48 +0100

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
