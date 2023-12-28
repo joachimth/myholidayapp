5db0b044e5803cc4d1e83c5947792d348695f3e6
Joachim Thirsbro <joachim@thirsbro.dk>
2023-12-28 15:34:15 +0100

    Footer.js updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-100 text-center p-4 mt-8">
       <p className="text-sm text-gray-600">
-        Repo: <a href="https://github.com/joachimth/MyHolidayApp" className="text-blue-600 hover:underline">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-blue-600 hover:underline">joachimth</a> | Senest Opdateret: {currentYear}
+        <a href="https://github.com/joachimth/MyHolidayApp" className="text-blue-600 hover:underline">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-blue-600 hover:underline">joachimth</a> | {currentYear}
       </p>
     </footer>
   );
