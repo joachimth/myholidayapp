6b53bb06b370434304d8a22c7633c071f30dbf43
Joachim Thirsbro <joachim@thirsbro.dk>
2024-01-14 21:27:19 +0100

    index.html updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     require('daisyui')
   ],
   daisyui: {
-    themes: ["acid"],
+    themes: ["acid", "light", "dark", "cupcake"],
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
