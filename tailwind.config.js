06f4a297514f9d21832895b83706cbf6c7d1a327
Joachim Thirsbro <joachim@thirsbro.dk>
2024-01-14 23:14:08 +0100

    tailwind.config.js updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,17 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('daisyui')
+    require("@tailwindcss/typography"),
+    require("daisyui")
   ],
   daisyui: {
-    themes: ["acid", "light", "dark", "cupcake"],
-    darkTheme: "acid", // name of one of the included themes for dark mode
+    themes: true,
+    darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root" // The element that receives theme color CSS variables
   },
 };
