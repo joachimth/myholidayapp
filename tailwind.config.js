a3ffed841194da107957a2bcdbbf4f9add2be5d9
Joachim Thirsbro <joachim@thirsbro.dk>
2024-01-14 19:39:30 +0100

    tailwind.config.js updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements-react/dist/js/**/*.js"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class", // or 'media' or false
+  darkMode: "class",
   plugins: [
-    require('daisyui'),
-    require("tw-elements-react/dist/plugin.cjs") // only if you're using tw-elements-react
+    require('daisyui')
   ],
   daisyui: {
     themes: ["acid"],
   },
-}
+};
