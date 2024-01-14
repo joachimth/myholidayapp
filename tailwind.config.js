c48306862c32db8b2f48bc0d1162e9da0b5f0156
Joachim Thirsbro <joachim@thirsbro.dk>
2024-01-14 20:54:44 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  darkMode: "class",
   plugins: [
     require('daisyui')
   ],
@@ -17,7 +16,7 @@ module.exports = {
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ":root", // The element that receives theme color CSS variables
   },
 };
