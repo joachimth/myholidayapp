412413b51cea78950b5b66be612596185a1942c2
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-20 18:14:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import 'daisyui/dist/full.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
