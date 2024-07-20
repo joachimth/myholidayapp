e7d49fea2b3c182f0800e82bb312ff7739616b76
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-20 17:35:45 +0200

    Update HolidayList.js

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -24,13 +24,6 @@ const HolidayList = () => {
 
   return (
     <div className="container mx-auto p-4">
-    
-    
-      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" onClick={setYear(currentYear)}>currentYear</button>
-      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" onClick={setYear(currentYear+1)}>currentYear+1</button>
-    
-    
-    
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="btn">
@@ -38,7 +31,7 @@ const HolidayList = () => {
             <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
           </Menu.Button>
         </div>
-        
+
         <Transition
           as={Fragment}
           enter="transition ease-out duration-100"
@@ -48,7 +41,7 @@ const HolidayList = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="py-1">
               {[currentYear, currentYear + 1, currentYear + 2].map(yr => (
                 <Menu.Item key={yr}>
