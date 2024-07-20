b76e59994d89c75749f16dfc92eadfd068415957
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-20 18:13:51 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'postcss-import': {}, // Ensure this plugin is installed and included
-    'postcss-nested': {}, // Ensure this plugin is installed and included
+    'postcss-import': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-nested': {}
   },
 }
