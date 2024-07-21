34a3dc0a96dee650908858b16db3c1e1b637f675
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 18:03:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import HolidayList from './components/HolidayList';
 import Footer from './components/Footer';
-import ThemeSelector from './components/ThemeSelector';
 
-function App() {
-  return (
-    <div className="min-h-screen flex flex-col bg-base-200 text-base-content">
-      <header className="bg-primary text-primary-content p-4 shadow-md">
-        <div className="flex justify-between items-center">
-          <h1 className="text-center text-4xl">My Holiday App</h1>
-        </div>
-        <div className="flex justify-between items-center">
-          <ThemeSelector />
-        </div>
-      </header>
-      <main className="flex-grow">
-        <HolidayList />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <header className="text-center py-4 bg-gray-100 shadow-md">
+      <h1 className="text-4xl font-bold text-gray-800">Danske Helligdage</h1>
+    </header>
+    <HolidayList />
+    <Footer />
+  </div>
+);
 
 export default App;
