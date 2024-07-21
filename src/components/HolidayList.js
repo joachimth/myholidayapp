60c39714cf860a6e0b1ca4ed45badea262a36aac
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 20:31:33 +0200

    Update HolidayList.js

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
+import ThemeSelector from './ThemeSelector';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -23,6 +24,12 @@ const HolidayList = () => {
   }, [year]);
 
   return (
+    <div className="mt-2">
+        <ThemeSelector />
+    </div>
+    
+    
+    
     <div className="container mx-auto p-4">
       <Menu as="div" className="relative inline-block text-left w-full">
         <div className="w-full">
