b8cfe960f5478001deb6c0c495dc688b77bd3db8
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 12:11:22 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require('daisyui')],
   daisyui: {
-    themes: ["acid"],
-  },
-}
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"]
+  }
+};
