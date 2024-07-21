3f5ae6a76bc13714d85c910ee8653c50b0ab7d0a
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 20:46:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import ThemeSelector from './components/ThemeSelector';
 
 const App = () => (
   <div>
-    <header className="text-center py-4 bg-gray-100 shadow-md">
-      <h1 className="text-4xl font-bold text-gray-800">Danske Helligdage</h1>
-    </header>
     <ThemeSelector />
     <HolidayList />
     <Footer />
