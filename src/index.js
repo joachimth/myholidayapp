56bee3d72270ded28400250f532e624e8c026d8f
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 09:46:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+//import './index.css';
+require("./index.css");
+
 
 ReactDOM.render(
   <React.StrictMode>
