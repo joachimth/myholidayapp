671a5c508b89f2243dc4c41582b0eeabad429e90
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 18:03:35 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import ThemeSelector from './ThemeSelector';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-100 text-center p-4 mt-8">
+    <footer className="bg-gray-100 text-center p-2 mt-8">
       <p className="text-sm text-gray-600">
-        <a href="https://github.com/joachimth/MyHolidayApp" className="text-blue-600 hover:underline">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-blue-600 hover:underline">joachimth</a> | {currentYear}
+        <a href="https://github.com/joachimth/myholidayapp" className="text-blue-600 hover:underline">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-blue-600 hover:underline">joachimth</a> | {currentYear}
       </p>
+      <div className="mt-2">
+        <ThemeSelector />
+      </div>
     </footer>
   );
 };
