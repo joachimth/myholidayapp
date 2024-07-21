4906b0cc7d6fc50b0097b63a17f501078bf6a6bc
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 18:03:55 +0200

    Update ThemeSelector.js

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -17,7 +17,7 @@ const ThemeSelector = () => {
     <Menu as="div" className="relative inline-block text-left w-full md:w-auto">
       <div className="w-full">
         <Menu.Button className="inline-flex w-full justify-between md:justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-          Select Theme
+          Vælg Tema
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
