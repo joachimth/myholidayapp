693faab526b70b11d0a72c2b1d20333f0029114e
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 20:42:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HolidayList from './components/HolidayList';
 import Footer from './components/Footer';
+import ThemeSelector from './components/ThemeSelector';
 
 const App = () => (
   <div>
     <header className="text-center py-4 bg-gray-100 shadow-md">
       <h1 className="text-4xl font-bold text-gray-800">Danske Helligdage</h1>
     </header>
+    <ThemeSelector />
     <HolidayList />
     <Footer />
   </div>
