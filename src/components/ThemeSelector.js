c0d16aa0b2064ae7a8e719b901adb764256898c7
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 13:30:36 +0200

    Update ThemeSelector.js

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
