4a40dec63a4d95892a1f73206cb2481ad83ebd61
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 20:23:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ThemeSelector from './components/ThemeSelector';
-import Footer from './components/Footer';
 
 // Set default theme to bumblebee
 document.documentElement.setAttribute('data-theme', 'bumblebee');
