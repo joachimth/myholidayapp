542d36a19af0ee4ad97413ffa003a6a522205148
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-21 13:41:01 +0200

    Update HolidayList.js

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -82,10 +82,6 @@ const HolidayList = () => {
 };
 
 const App = () => (
-  <div className="min-h-screen flex flex-col bg-base-200 text-base-content">
-    <header className="bg-primary text-primary-content p-4 shadow-md">
-      <h1 className="text-center text-4xl">My Holiday App</h1>
-    </header>
     <main className="flex-grow">
       <HolidayList />
     </main>
