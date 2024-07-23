58fadf007da77e67c226cf2e756260814b330f4b
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-24 01:01:57 +0200

    Update HolidayList.js

--- src/components/HolidayList.js
+++ src/components/HolidayList.js
@@ -36,7 +36,7 @@ const HolidayList = () => {
   return (
     <>
       <div className="navbar bg-primary text-primary-content mb-4">
-        <span className="btn btn-ghost normal-case text-xl">Danske Helligdage i {year}</span>
+        <button className="btn btn-ghost normal-case text-xl">Danske Helligdage i {year}</button>
       </div>
 
       <div className="container mx-auto p-4">
@@ -95,7 +95,7 @@ const HolidayList = () => {
 
       {modalData && (
         <Modal isOpen={!!modalData} onRequestClose={closeModal} contentLabel="Holiday Info">
-          <div className="p-4">
+          <div className="modal-content p-4">
             <h2 className="text-lg font-bold">{modalData.title}</h2>
             <p className="mt-2">{modalData.description}</p>
             <p className="mt-2">
