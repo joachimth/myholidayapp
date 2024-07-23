e1e69d3cb52e3b444794e27fb6b9af799d1681b9
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-24 00:46:13 +0200

    Update holidayInfo.js

--- src/services/holidayInfo.js
+++ src/services/holidayInfo.js
@@ -30,4 +30,43 @@ const holidayDetails = {
     description: "2. påskedag er dagen efter påskedag.",
     icon: faStar,
     reference: "https://da.wikipedia.org/wiki/P%C3%A5ske"
- ​⬤
+  },
+  "Kristi himmelfartsdag": {
+    title: "Kristi himmelfartsdag",
+    description: "Kristi himmelfartsdag markerer Jesu himmelfart.",
+    icon: faStar,
+    reference: "https://da.wikipedia.org/wiki/Kristi_himmelfartsdag"
+  },
+  "Pinsedag": {
+    title: "Pinsedag",
+    description: "Pinsedag fejrer Helligåndens komme.",
+    icon: faFire,
+    reference: "https://da.wikipedia.org/wiki/Pinse"
+  },
+  "2. pinsedag": {
+    title: "2. pinsedag",
+    description: "2. pinsedag er dagen efter pinsedag.",
+    icon: faFire,
+    reference: "https://da.wikipedia.org/wiki/Pinse"
+  },
+  "Juleaften": {
+    title: "Juleaften",
+    description: "Juleaften fejrer Jesu fødsel.",
+    icon: faTree,
+    reference: "https://da.wikipedia.org/wiki/Juleaften"
+  },
+  "Juledag": {
+    title: "Juledag",
+    description: "Juledag fejrer Jesu fødsel.",
+    icon: faGift,
+    reference: "https://da.wikipedia.org/wiki/Juledag"
+  },
+  "2. juledag": {
+    title: "2. juledag",
+    description: "2. juledag er dagen efter juledag.",
+    icon: faGift,
+    reference: "https://da.wikipedia.org/wiki/Juledag"
+  },
+};
+
+export default holidayDetails;
