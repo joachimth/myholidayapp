2a7d4aa33475a9bf02859121c1511cf6bdcab65d
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-23 22:35:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import ThemeSelector from './components/ThemeSelector';
 function App() {
   return (
     <div className="min-h-screen bg-base-100 text-base-content">
+      <ThemeSelector />
+      <div className="mb-8"> {/* Juster margin i bunden her */}
       <HolidayList />
       <Footer />
-      <ThemeSelector />
     </div>
   );
 }
