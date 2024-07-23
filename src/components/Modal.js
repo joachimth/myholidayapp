d785175f4b77ad2ecb4e4c1957e7fda1e265a4ac
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-24 01:02:24 +0200

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import Modal from 'react-modal';
+import ReactModal from 'react-modal';
 
-Modal.setAppElement('#root');
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '90%', // Adjusts the max-width for better responsiveness
+    width: '100%',
+    padding: '20px', // Adds padding for better spacing
+  },
+};
 
-const CustomModal = ({ isOpen, onRequestClose, contentLabel, children }) => (
-  <Modal
+ReactModal.setAppElement('#root');
+
+const Modal = ({ isOpen, onRequestClose, contentLabel, children }) => (
+  <ReactModal
     isOpen={isOpen}
     onRequestClose={onRequestClose}
+    style={customStyles}
     contentLabel={contentLabel}
-    className="modal"
-    overlayClassName="modal-overlay"
   >
-    {children}
-  </Modal>
+    <div className="flex flex-col items-center">
+      <button
+        onClick={onRequestClose}
+        className="btn btn-sm btn-secondary self-end mb-4"
+      >
+        Luk
+      </button>
+      {children}
+    </div>
+  </ReactModal>
 );
 
-export default CustomModal;
+export default Modal;
