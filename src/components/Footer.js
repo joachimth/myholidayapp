caa4438887f2c3636a8b528af47dc464e66ef95e
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-23 22:46:52 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-base-200 text-base-content text-center p-4 mt-8">
-      <p className="text-sm mt-2">
-        <a href="https://github.com/joachimth/MyHolidayApp" className="text-primary hover:underline">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-primary hover:underline">joachimth</a> | {currentYear}
+    <footer className="bg-neutral text-neutral-content text-center p-4 mt-8">
+      <p className="text-sm">
+        <a href="https://github.com/joachimth/myholidayapp" className="text-primary hover:underline">myHolidayApp</a> by <a href="https://github.com/joachimth" className="text-primary hover:underline">joachimth</a> | {currentYear}
       </p>
     </footer>
   );
