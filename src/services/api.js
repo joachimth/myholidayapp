647c643ca551918395ae6d7d569df967b0d61507
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-23 23:58:21 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const fetchHolidays = async (year) => {
+export const fetchHolidays = async (year) => {
   try {
     const response = await axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year}/DK`);
     return response.data;
   } catch (error) {
-    console.error('Error:', error);
-    return [];
+    console.error('Error fetching holidays:', error);
+    throw error;
   }
 };
-
-export { fetchHolidays };
