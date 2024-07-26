435aebf47a7c434fc35e32d281d4d7d61a2b16b3
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-07-26 21:06:34 +0200

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,41 +1,15 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
-const customStyles = {
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
-  },
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: '90%', // Adjusts the max-width for better responsiveness
-    width: '100%',
-    padding: '20px', // Adds padding for better spacing
-  },
-};
-
-ReactModal.setAppElement('#root');
-
-const Modal = ({ isOpen, onRequestClose, contentLabel, children }) => (
+const Modal = ({ isOpen, onRequestClose, children }) => (
   <ReactModal
     isOpen={isOpen}
     onRequestClose={onRequestClose}
-    style={customStyles}
-    contentLabel={contentLabel}
+    contentLabel="Holiday Details"
+    className="modal"
+    overlayClassName="modal-overlay"
   >
-    <div className="flex flex-col items-center">
-      <button
-        onClick={onRequestClose}
-        className="btn btn-sm btn-secondary self-end mb-4"
-      >
-        Luk
-      </button>
-      {children}
-    </div>
+    {children}
   </ReactModal>
 );
 
